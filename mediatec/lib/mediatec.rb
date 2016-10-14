1cb4ca643a5869c54de76b6bbd24af53157e9cdf
Loic1204 <loicderyckere@hotmail.fr>
2016-10-13 20:51:23 -0400

    Creation des fonctions search, check et addusr

--- mediatec/lib/mediatec.rb
+++ mediatec/lib/mediatec.rb
@@ -1,4 +1,4 @@
 require 'mediatec/version.rb'
-
+require 'mediatec/mediatec.rb'
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
