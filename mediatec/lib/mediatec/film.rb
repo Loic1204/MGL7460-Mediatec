cfdd48e7b75d55db38c737c9a4e70d6c1d15be71
Loic1204 <loicderyckere@hotmail.fr>
2016-10-15 12:22:26 -0400

    Update film.rb

--- mediatec/lib/mediatec/film.rb
+++ mediatec/lib/mediatec/film.rb
@@ -2,8 +2,10 @@ class Film
 	attr_reader :titre
 	attr_reader :realisateur
 	attr_reader :date
+	attr_reader :emprunt
+	attr_reader :reservation
 	
-	def initialize (titre, realisateur, date)
-		@titre, @realisateur, @date = titre, realisateur, date
+	def initialize (titre, realisateur, date, emprunt, reservation)
+		@titre, @realisateur, @date, @emprunt, @reservation = titre, realisateur, date, emprunt, reservation
 	end
 end
