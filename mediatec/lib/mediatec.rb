3f21578be73ed28b0b4f95fd279b288fe3294a8a
AchkaN0n1x <sarah.svalero@gmail.com>
2016-10-22 12:01:30 -0400

    Corrected files name

--- mediatec/lib/mediatec.rb
+++ mediatec/lib/mediatec.rb
@@ -2,7 +2,7 @@ require 'mediatec/version.rb'
 require 'mediatec/mediatec.rb'
 require 'mediatec/user.rb'
 require 'mediatec/film.rb'
-require 'mediatec/fluxuser.rb'
-require 'mediatec/fluxfilm.rb'
+require 'mediatec/flux_user.rb'
+require 'mediatec/flux_film.rb'
 # Add requires for other files you add to your project here, so
 # you just need to require this one file in your bin file
